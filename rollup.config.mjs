ba9c167d834962d11531565678cb6b64eb94b8c3
McOtech <otienopaul36@gmail.com>
2024-10-16 09:18:08 +0300

    feat: integrated antd

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,9 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
-// import terser from '@rollup/plugin-terser';
-// import json from '@rollup/plugin-json';
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -25,15 +25,15 @@ export default [
       },
     ],
     plugins: [
-      // peerDepsExternal(),
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({
         tsconfig: './tsconfig.json',
       }),
       postcss(),
-      // terser(),
-      // json(),
+      terser(),
+      json(),
     ],
   },
   {
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Button.css';
+import { Button as AntdButton, Col, Row } from 'antd';
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <>
+      <Row gutter={16}>
+        <Col md={12} xs={24}>
+          <AntdButton type="primary">Primary Button</AntdButton>
+        </Col>
+        <Col md={12} xs={24}>
+          <button>{props.label}</button>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Button;
