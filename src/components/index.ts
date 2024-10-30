44d15d2d3c151e9138b3c548968502e5a18c74a5
Paul Otieno <otienopaul36@gmail.com>
2024-10-30 15:27:27 +0300

    fix: exported update application process component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as CreateApplicationForm } from './CreateApplicationForm';
+export { default as UpdateApplicationForm } from './UpdateApplicationForm';
 export { default as useSbNotification } from './Notification';
 export { default as MyApplications } from './MyApplications';
 export { default as WebInstallButtonHOC } from './WebInstallButton';
