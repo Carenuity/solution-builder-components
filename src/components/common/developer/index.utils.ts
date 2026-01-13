86c8cd5866e31ce2da064e2d27639175d55dd2fd
Paul Otieno <otienopaul36@gmail.com>
2026-01-13 14:03:39 +0300

    refactor: featured other supported chip familieson getBoardChipFamily function

--- src/components/common/developer/index.utils.ts
+++ src/components/common/developer/index.utils.ts
@@ -14,12 +14,37 @@ export const getBoardChipFamily = (
   boardName?: string
 ): ChipFamily | undefined => {
   if (boardName) {
-    if (boardName.includes('D1')) {
-      return 'ESP8266';
-    } else if (boardName.includes('C3')) {
-      return 'ESP32-C3';
-    } else if (boardName.trim() === 'ESP32' || boardName.includes('ESP32')) {
-      return 'ESP32';
+    // if (boardName.includes('D1')) {
+    //   return 'ESP8266';
+    // } else if (boardName.includes('C3')) {
+    //   return 'ESP32-C3';
+    // } else if (boardName.trim() === 'ESP32' || boardName.includes('ESP32')) {
+    //   return 'ESP32';
+    // }
+    switch (true) {
+      case boardName.includes('C2'):
+        return 'ESP32-C2';
+
+      case boardName.includes('C3'):
+        return 'ESP32-C3';
+
+      case boardName.includes('C6'):
+        return 'ESP32-C6';
+
+      case boardName.includes('H2'):
+        return 'ESP32-H2';
+
+      case boardName.includes('S2'):
+        return 'ESP32-S2';
+
+      case boardName.includes('S3'):
+        return 'ESP32-S3';
+
+      case boardName.includes('D1'):
+        return 'ESP8266';
+
+      case boardName.includes('CAM') || boardName.trim() === 'ESP32':
+        return 'ESP32';
     }
   }
   return undefined;
--- src/utils/types.utils.ts
+++ src/utils/types.utils.ts
@@ -27,7 +27,15 @@ export type IApiErrorResponse = {
   response: IErrorResponseObject;
 };
 
-export type ChipFamily = 'ESP8266' | 'ESP32-C3' | 'ESP32';
+export type ChipFamily =
+  | 'ESP8266'
+  | 'ESP32'
+  | 'ESP32-C2'
+  | 'ESP32-C3'
+  | 'ESP32-C6'
+  | 'ESP32-H2'
+  | 'ESP32-S2'
+  | 'ESP32-S3';
 export type BinaryFileId = 'boot' | 'bootloader' | 'partitions' | 'main';
 export type BinaryFormat = 'merged' | 'arduino_parts';
 
